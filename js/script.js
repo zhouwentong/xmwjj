7a61c33e62b8fa916c107e13d1a23cc45b874efa
zhouwentong <vintone1984@gmail.com>
2013-06-14 10:59:44 +0800

    update
    xmwjj

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	$('#h .nav .l1').hover(function(){
+		$(this).addClass('active1').children('ul').css('display','block');
+		},function(){
+			$(this).removeClass('active1').children('ul').css('display','none');
+			})
+	$('#b .nav1 li').click(function(e) {
+        $(this).addClass('active2').siblings().removeClass('active2');
+    });
+	$(".m2").click(function(e) {
+            $("body, html").animate({scrollTop:0},1000);
+			return false;
+        });
+})
